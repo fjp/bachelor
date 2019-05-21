e9c537c3fe816ba62a1f9914e95cbd8f88e74b3a
Franz Pucher <franz.pucher@gmail.com>
2019-05-22 01:14:28 +0200

    adds gtest and implements basic test

--- main.cpp
+++ main.cpp
@@ -1,106 +1,12 @@
-#include "visualizer.h"
-#include <fstream>
-#include <string>
-#include <vector>
-#include <exception>
 
-
-
-
-#include "planner.h"
-#include "structs.h"
-#include "audi_rover.h"
-
-#include "location.h"
-
-using namespace planner;
-
-#ifdef _MSC_VER
-static const char* PATH_SEP = "\\";
-#else
-static const char* PATH_SEP = "/";
-#endif
 
 #define DEBUG_FILES
 
-// Bits used in the overrides image bytes
-enum OverrideFlags
-{
-    OF_RIVER_MARSH = 0x10,
-    OF_INLAND = 0x20,
-    OF_WATER_BASIN = 0x40,
-    OF_PATH = 0x01,
-};
-
-// Some constants
-enum {
-    IMAGE_DIM = 2048, // Width and height of the elevation and overrides image
-    
-    ROVER_X = 159,
-    ROVER_Y = 1520,
-    BACHELOR_X = 1303,
-    BACHELOR_Y = 85,
-    WEDDING_X = 1577,
-    WEDDING_Y = 1294
-};
-
-std::ifstream::pos_type fileSize(const std::string& filename)
-{
-    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
-    if (!in.good())
-    {
-        throw std::exception();
-    }
-    return in.tellg(); 
-}
-
-std::vector<uint8_t> loadFile(const std::string& filename, size_t expectedFileSize)
-{
-    size_t fsize = fileSize(filename);
-    if (fsize != expectedFileSize)
-    {
-        throw std::exception();
-    }
-    std::vector<uint8_t> data(fsize);
-    std::ifstream ifile(filename, std::ifstream::binary);
-    if (!ifile.good())
-    {
-        throw std::exception();
-    }
-    ifile.read((char*)&data[0], fsize);
-    return data;
-}
-
-bool donut(int x, int y, int x1, int y1)
-{
-    int dx = x - x1;
-    int dy = y - y1;
-    int r2 = dx * dx + dy * dy;
-    return r2 >= 150 && r2 <= 400;
-}
-
-
-bool path(int x, int y, uint8_t* overrides)
-{
-
-    for (int i = -3; i < 3; ++i)
-    {
-        int dx = x - i;
-        for (int j = -3; j < 3; ++j)
-        {
-            int dy = y - j;
-            if ((dx >= 0 && dx < IMAGE_DIM) && (dy >= 0 && dy < IMAGE_DIM) && (overrides[dy * IMAGE_DIM + dx] & (OF_PATH)))
-            {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
+#include "utilities.h"
 
 int main(int argc, char** argv)
 {
+    /// Prepare to read elevation and overrides data
     const size_t expectedFileSize = IMAGE_DIM * IMAGE_DIM;
     // Address assets relative to application location
     std::string anchor = std::string(".") + PATH_SEP;
@@ -128,11 +34,11 @@ int main(int argc, char** argv)
     oAudiRover.Summon();
 
 
-    oAudiRover.SetStart({BACHELOR_X, BACHELOR_Y});
-    oAudiRover.SetGoal({WEDDING_X, WEDDING_Y});
+    //oAudiRover.SetStart({BACHELOR_X, BACHELOR_Y});
+    //oAudiRover.SetGoal({WEDDING_X, WEDDING_Y});
 
 
-    oAudiRover.Summon();
+    //oAudiRover.Summon();
 
 
 
--- planner/test/test_fixture.h
+++ planner/test/test_fixture.h
@@ -5,4 +5,124 @@
 #ifndef BACHELORTEST_TEST_FIXTURE_H
 #define BACHELORTEST_TEST_FIXTURE_H
 
+#include "utilities.h"
+
+#include <stdio.h>  /* defines FILENAME_MAX */
+
+
+// The fixture for testing class Foo.
+class cPlannerTest : public ::testing::Test {
+protected:
+    // You can remove any or all of the following functions if its body
+    // is empty.
+
+    cPlannerTest() {
+        // You can do set-up work for each test here.
+    }
+
+    ~cPlannerTest() override {
+        // You can do clean-up work that doesn't throw exceptions here.
+    }
+
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
+
+    void SetUp() override {
+        // Code here will be called immediately after the constructor (right
+        // before each test).
+        //std::cout << "SetUp Tests" << std::endl;
+
+
+#ifdef WINDOWS
+        #include <direct.h>
+    #define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+
+        char cCurrentPath[FILENAME_MAX];
+
+        if (!GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
+        {
+            //return errno;
+        }
+
+        cCurrentPath[sizeof(cCurrentPath) - 1] = '\0'; /* not really required */
+
+        //printf ("The current working directory is %s\n", cCurrentPath);
+
+        /// Prepare to read elevation and overrides data
+        const size_t expectedFileSize = IMAGE_DIM * IMAGE_DIM;
+        // Address assets relative to application location
+        std::string anchor = std::string(".") + PATH_SEP;
+        std::string pname = cCurrentPath;
+        auto lastpos = pname.find_last_of("/\\");
+        if (lastpos != std::string::npos) {
+            anchor = pname.substr(0, lastpos);
+        }
+        for (int i = 0; i < 2; ++i) {
+            auto lastpos = anchor.find_last_of("/\\");
+            if (lastpos != std::string::npos) {
+                anchor = anchor.substr(0, lastpos);
+            }
+        }
+        anchor += PATH_SEP;
+        m_oElevation = loadFile(anchor + "assets" + PATH_SEP + "elevation.data", expectedFileSize);
+        m_oOverrides = loadFile(anchor + "assets" + PATH_SEP + "overrides.data", expectedFileSize);
+        m_oImage = std::ofstream("pic.bmp", std::ofstream::binary);
+    }
+
+    void TearDown() override {
+        // Code here will be called immediately after each test (right
+        // before the destructor).
+        visualizer::writeBMP(
+                m_oImage,
+                &m_oElevation[0],
+                IMAGE_DIM,
+                IMAGE_DIM,
+                [&] (size_t x, size_t y, uint8_t elevation) {
+
+                    // Marks interesting positions on the map
+                    if (donut(x, y, ROVER_X, ROVER_Y) ||
+                        donut(x, y, BACHELOR_X, BACHELOR_Y) ||
+                        donut(x, y, WEDDING_X, WEDDING_Y))
+                    {
+                        return uint8_t(visualizer::IPV_PATH);
+                    }
+
+                    if (path(x, y, &m_oOverrides[0]))
+                    {
+                        return uint8_t(visualizer::IPV_PATH);
+                    }
+
+                    // Signifies water
+                    if ((m_oOverrides[y * IMAGE_DIM + x] & (OF_WATER_BASIN | OF_RIVER_MARSH)) ||
+                        elevation == 0)
+                    {
+                        return uint8_t(visualizer::IPV_WATER);
+                    }
+
+                    // Signifies normal ground color
+                    if (elevation < visualizer::IPV_ELEVATION_BEGIN)
+                    {
+                        elevation = visualizer::IPV_ELEVATION_BEGIN;
+                    }
+                    return elevation;
+                });
+        m_oImage.flush();
+#if __APPLE__
+        auto res = system("open pic.bmp");
+        (void)res;
+#endif
+
+    }
+
+    // Objects declared here can be used by all tests in the test case for Foo.
+    std::vector<uint8_t> m_oElevation;
+    std::vector<uint8_t> m_oOverrides;
+
+    std::ofstream m_oImage;
+};
+
 #endif //BACHELORTEST_TEST_FIXTURE_H
