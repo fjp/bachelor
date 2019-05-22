511414ef9a4cf8e91ea722101662dff7a6591c42
F Pucher <franz.pucher@gmail.com>
2019-05-22 10:48:55 +0200

    adds documentation and changes signatures

--- planner/include/planner.h
+++ planner/include/planner.h
@@ -37,23 +37,43 @@ namespace planner {
 
         std::vector<std::vector<int> > m_mnHeuristic;
     public:
-        const int &Heuristic(const uint i_nX, const uint i_nY) const;
-        const int &Heuristic(const tLocation &i_sLocation) const;
+        const int32_t &Heuristic(const uint i_nX, const uint i_nY) const;
+        const int32_t &Heuristic(const tLocation &i_sLocation) const;
 
         //bool GoalTest(const tNode &i_sFirst, const tNode &i_sSecond) const override;
         //tNode Child(tNode &i_sParent, const tAction &i_sAction) override;
 
+        ///\brief Test if the provided location i_sLocation lies within the map
+        ///\details Checks if the provided location lies within the map height and width
+        ///         and if the location is equal or greater than zero.
         bool WithinMap(const tLocation &i_sLocation) const;
 
+        ///\brief Goal test to check if the two provided nodes i_sFirst, i_sSecond are equal.
+        ///\details Note that this check takes the step size m_nStepSize of the rover into account.
+        ///\param[in] i_sFirst could be the current node that needs to be checked.
+        ///\param[in] i_sSecond could be the goal node.
+        ///\return true or false if the two nodes are equal.
         bool GoalTest(const tNode *i_sFirst, const tNode *i_sSecond) const override;
 
-        tNode* Child(tNode *i_sParent, const tAction &i_sAction) override;
+        ///\brief Generate a successor node state given a node i_sParent and action i_sAction.
+        ///\details Overrides method of the base class inteface cPlannerInterface<size_t Directions>.
+        ///         Defines a new node on the heap and initializes it according to the given action.
+        ///
+        ///\param[in] i_sParent node which becomes the parent of the new node.
+        ///\param[in] i_sAction struct of type tAction that contains the direction and cost of the action.
+        tNode* Child(tNode *i_sParent, const tAction &i_sAction) const override;
 
     private:
 
-        int GradX(int i_nX, int i_nY);
-        int GradY(int i_nX, int i_nY);
+        ///\brief Calculates the discrete gradient of the map m_poMap in x direction.
+        const int32_t GradX(uint32_t i_nX, uint32_t i_nY) const;
         
+        ///\brief Calculates the discrete gradient of the map m_poMap in y direction.
+        const int32_t GradY(uint32_t i_nX, uint32_t i_nY) const;
+        
+        ///\brief Calculates the node hash using its location and the width of the map
+        ///\details The hash is required to sort the std::map<tNode> oCost of reaching a node,
+        ///         which is used in the Astar() search algorithm.
         uint32_t NodeHash(const tNode *i_sNode);
 
     };
