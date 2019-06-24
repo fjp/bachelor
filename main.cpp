306fe614bf94d07a729f4c898a81776dd4466929
Franz Pucher <franz.pucher@gmail.com>
2019-06-24 22:04:12 +0200

    refactors main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "utilities.h"
 #include "visualizer.h"
+#include "result.h"
 
 #include <iostream>
 
@@ -34,66 +35,23 @@ int main(int argc, char** argv)
     poAudiRover->SetGoal(tLocation{BACHELOR_X, BACHELOR_Y});
 
 
-    poAudiRover->Summon(1, 1, ASTAR);
+    tResult sResultR2B = poAudiRover->Summon(1, 1, "ASTAR");
 
 
     poAudiRover->SetStart({BACHELOR_X, BACHELOR_Y});
     poAudiRover->SetGoal({WEDDING_X, WEDDING_Y});
 
 
-    poAudiRover->Summon(1, 1, ASTAR);
+    tResult sResultB2W = poAudiRover->Summon(1, 1, "ASTAR");
 
 
     /// Report the total planning time
-
     double fIslandSeconds = poAudiRover->TotalTime();
     std::cout << "\nTravelling will take " << fIslandSeconds << " island seconds ("
               << fIslandSeconds/60.f << " island minutes or " << fIslandSeconds/60.f/60.f << " island hours) on the fastest path. " << std::endl;
 
     //////////// Output the found path /////////////////////
-    visualizer::write("pic_final.bmp", &elevation[0], &overrides[0], asLocation, IMAGE_DIM);
-    /*
-    visualizer::writeBMP(
-        of,
-        &elevation[0],
-        IMAGE_DIM,
-        IMAGE_DIM,
-        [&] (size_t x, size_t y, uint8_t elevation) {
-
-            // Marks interesting positions on the map
-            if (visualizer::donut(x, y, ROVER_X, ROVER_Y) ||
-                visualizer::donut(x, y, BACHELOR_X, BACHELOR_Y) ||
-                visualizer::donut(x, y, WEDDING_X, WEDDING_Y))
-            {
-                return uint8_t(visualizer::IPV_PATH);
-            }
-
-            if (visualizer::path(x, y, &overrides[0]))
-            {
-                return uint8_t(visualizer::IPV_PATH);
-            }
-
-            // Signifies water
-            if ((overrides[y * IMAGE_DIM + x] & (OF_WATER_BASIN | OF_RIVER_MARSH)) ||
-                elevation == 0)
-            {
-                return uint8_t(visualizer::IPV_WATER);
-            }
-
-            if (visualizer::visited(x, y, &overrides[0]))
-            {
-                return uint8_t(visualizer::IPV_VISITED);
-            }
-
-            // Signifies normal ground color
-            if (elevation < visualizer::IPV_ELEVATION_BEGIN)
-            {
-                elevation = visualizer::IPV_ELEVATION_BEGIN;
-            }
-            return elevation;
-    });
-    of.flush();
-     */
+    visualizer::write("pic.bmp", &elevation[0], &overrides[0], asLocation, IMAGE_DIM);
 #if __APPLE__
     auto res = system("open pic.bmp");
     (void)res;
